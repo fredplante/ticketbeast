c5152b17c7a9e9c8335e4ed8f78aac5263ffe743
Frédéric Planté <f.plante@stuart.com>
2019-12-02 15:49:55 +0100

    16-Reducing Duplication with Custom Assertions

--- app/forms/purchase_tickets_form.rb
+++ app/forms/purchase_tickets_form.rb
@@ -4,4 +4,7 @@ class PurchaseTicketsForm
   attr_accessor :email, :ticket_quantity, :payment_token
 
   validates :email, presence: true
+  validates :email,  format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, allow_blank: true
+  validates :ticket_quantity, numericality: { only_integer: true, greater_than: 0 }
+  validates :payment_token, presence: true
 end
--- test/integration/purchase_tickets_test.rb
+++ test/integration/purchase_tickets_test.rb
@@ -9,12 +9,12 @@ class PurchaseTicketsTest < ActionDispatch::IntegrationTest
     concert = create(:concert, ticket_price: 3250)
     PaymentGateway.expects(:create_adapter).returns(@payment_gateway)
 
-    url = "/concerts/#{concert.id}/orders"
-    params = { email: "john@example.com", ticket_quantity: 3, payment_token: @payment_gateway.valid_test_token }
-    post url, params: params, as: :json
+    order_tickets(concert, {
+      email: "john@example.com", ticket_quantity: 3,
+                                   payment_token: @payment_gateway.valid_test_token
+    })
 
     assert_response :created
-
     assert_equal 9750, @payment_gateway.total_charges
 
     order = concert.orders.where(email: "john@example.com").first
@@ -24,12 +24,61 @@ class PurchaseTicketsTest < ActionDispatch::IntegrationTest
 
   test "email is required to purchase tickets" do
     concert = create(:concert)
-    url = "/concerts/#{concert.id}/orders"
-    params = { ticket_quantity: 3, payment_token: @payment_gateway.valid_test_token }
 
-    post url, params: params, as: :json
+    order_tickets(concert, {
+      ticket_quantity: 3, payment_token: @payment_gateway.valid_test_token })
 
+    assert_validation_error("email")
+  end
+
+  test "email must be valid to purchase tickets" do
+    concert = create(:concert)
+
+    order_tickets(concert, {
+      email: "not-a-valid-email", ticket_quantity: 3,
+                                    payment_token: @payment_gateway.valid_test_token
+    })
+
+    assert_validation_error("email")
+  end
+
+  test "ticket quantity is required to purchase tickets" do
+    concert = create(:concert)
+
+    order_tickets(concert, {
+      email: "john@example.com", payment_token: @payment_gateway.valid_test_token
+    })
+
+    assert_validation_error("ticket_quantity")
+  end
+
+  test "ticket quantity must be at least 1 to purchase tickets" do
+    concert = create(:concert)
+
+    order_tickets(concert, {
+      email: "john@example.com", ticket_quantity: 0,
+                                   payment_token: @payment_gateway.valid_test_token
+    })
+
+    assert_validation_error("ticket_quantity")
+  end
+
+  test "payment_token is required" do
+    concert = create(:concert)
+
+    order_tickets(concert, { email: "john@example.com", ticket_quantity: 1 })
+
+    assert_validation_error("payment_token")
+  end
+
+  private
+
+  def assert_validation_error(attribute)
     assert_response :unprocessable_entity
-    assert JSON.parse(response.body).has_key? "email"
+    assert JSON.parse(response.body).has_key? attribute
+  end
+
+  def order_tickets(concert, params)
+    post "/concerts/#{concert.id}/orders", params: params, as: :json
   end
 end
