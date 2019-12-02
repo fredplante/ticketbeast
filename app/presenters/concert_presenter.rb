01e201aa5aa368f898fd0e8a93d33208236db3e8
Frédéric Planté <f.plante@stuart.com>
2019-12-02 09:25:25 +0100

    5-Refactoring for Speed

--- app/presenters/concert_presenter.rb
+++ app/presenters/concert_presenter.rb
@@ -1,8 +1,19 @@
 class ConcertPresenter < BasePresenter
   presents :concert
 
+  delegate :title, :subtitle, :venue, :venue_address, :city, :state,
+           :zip, :additional_information, to: :concert
+
   def formatted_date
     concert.date.strftime("%B %-d, %Y")
+  end
+
+  def formatted_start_time
+    concert.date.strftime("%-l:%M%P")
+  end
+
+  def ticket_price_in_dollars
+    number_with_precision(concert.ticket_price.to_f / 100, precision: 2)
   end
 
 end
--- test/presenters/concert_presenter_test.rb
+++ test/presenters/concert_presenter_test.rb
@@ -3,8 +3,20 @@ class ConcertPresenterTest < ActionView::TestCase
     concert = build(:concert, date: Time.zone.parse("2016-12-01 8:00pm"))
     concert_presenter = ConcertPresenter.new(concert, view)
 
-    date = concert_presenter.formatted_date
+    assert_equal "December 1, 2016", concert_presenter.formatted_date
+  end
 
-    assert_equal "December 1, 2016", date
+  test "can get formatted start time" do
+    concert = build(:concert, date: Time.zone.parse("2016-12-01 17:00:00"))
+    concert_presenter = ConcertPresenter.new(concert, view)
+
+    assert_equal "5:00pm", concert_presenter.formatted_start_time
+  end
+
+  test "can get ticket price in dollars" do
+    concert = build(:concert, ticket_price: 6750)
+    concert_presenter = ConcertPresenter.new(concert, view)
+
+    assert_equal "67.50", concert_presenter.ticket_price_in_dollars
   end
 end
