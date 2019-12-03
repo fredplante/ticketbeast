d25585c785ee9c8ed7f09ed7670330062f94ba15
Frédéric Planté <f.plante@stuart.com>
2019-12-03 18:35:20 +0100

    38-Hooking into Charges

--- app/models/payment_gateway/fake_adapter.rb
+++ app/models/payment_gateway/fake_adapter.rb
@@ -1,13 +1,22 @@
 module PaymentGateway
   class FakeAdapter
+    extend ActiveModel::Callbacks
+    define_model_callbacks :charge
+
+    attr_accessor :before_charge_callback
+
+    before_charge :run_before_charge_callback
+
     def initialize
       @charges = []
     end
 
     def charge(amount, token)
-      raise PaymentFailedError.new("Invalid token") if token != valid_test_token
+      run_callbacks :charge do
+        raise PaymentFailedError.new("Invalid token") if token != valid_test_token
 
-      @charges << amount
+        @charges << amount
+      end
     end
 
     def total_charges
@@ -16,6 +25,14 @@ module PaymentGateway
 
     def valid_test_token
       "valid-test-token"
+    end
+
+    private
+
+    def run_before_charge_callback
+      return unless before_charge_callback.is_a? Proc
+
+      before_charge_callback.call
     end
   end
 end
--- test/models/payment_gateway/fake_adapter_test.rb
+++ test/models/payment_gateway/fake_adapter_test.rb
@@ -16,4 +16,18 @@ class FakeAdapterTest < ActiveSupport::TestCase
       payment_gateway.charge(2500, "invalid-payment-token")
     }
   end
+
+  test "running a hook before the first charge" do
+    payment_gateway = PaymentGateway::FakeAdapter.new
+    callback_ran = false
+
+    payment_gateway.before_charge_callback = proc {
+      callback_ran = true
+      assert_equal 0, payment_gateway.total_charges
+    }
+
+    payment_gateway.charge(2500, payment_gateway.valid_test_token)
+    assert_equal 2500, payment_gateway.total_charges
+    assert callback_ran
+  end
 end
